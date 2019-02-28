a4316de2d3fb628a2910a226b10fb34312ef0d7a
Christopher Jacobs <chrisj.3222@gmail.com>
2019-02-28 01:00:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 
 def line(array)
-  sentence_array = ["The line is currently: "]
+  sentence_array = ["The line is currently:"]
   if array.count == 0
     puts "The line is currently empty."
   else
